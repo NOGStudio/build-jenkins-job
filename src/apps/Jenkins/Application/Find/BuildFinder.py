30272ff8ccc544048696e7dcbb4bd7060f20c01a
Vince' Dubois <38558569+MrVinceZ@users.noreply.github.com>
2024-05-28 00:33:00 +0200

    Update BuildFinder.py
    Try catch on get build console output call

--- src/apps/Jenkins/Application/Find/BuildFinder.py
+++ src/apps/Jenkins/Application/Find/BuildFinder.py
@@ -1,7 +1,6 @@
 from src.apps.Jenkins.Domain.ServerRepository import ServerRepository
 from typing import Type
 
-
 class BuildFinder:
     def __init__(self, repository: Type[ServerRepository], name: str) -> None:
         self.repository = repository
@@ -9,8 +8,12 @@ class BuildFinder:
         self.consoleLines = 0
 
     def exec(self, number: int) -> str:
+        try:
+            consoleLines = self.repository.get_build_console_output(self.name, number).split("\n")
+        except Exception as e:
+            print(f"An error occurred while getting console output: {e}. Continuing execution as non-fatal.")
+            consoleLines = []
 
-        consoleLines = self.repository.get_build_console_output(self.name, number).split("\n")
         consoleLinesCount = len(consoleLines)
         for i in range(self.consoleLines, consoleLinesCount):
             print(consoleLines[i])
@@ -20,4 +23,4 @@ class BuildFinder:
         return self.repository.get_status(number)
     
     def number(self) -> int:
-        return self.repository.get_number()    
+        return self.repository.get_number()
